9e62cfcf61f3d34d0357fa849f2e897ebdae1743
samke-glitch <sammythemwa@gmail.com>
2024-03-04 11:46:39 +0300

    conditions

---
+++ march/conditionS.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+condition statement example
+"""
+
+albumYear = int(input("album year: "))
+if(albumYear > 1970) and (albumYear < 1990):
+    print("Album year was in netween 1980 and 1989")
+print("")
+print("So Stuff..")
