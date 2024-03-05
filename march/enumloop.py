9765faf3101933dbb6e696d09a3e953b917d275d
samke-glitch <sammythemwa@gmail.com>
2024-03-05 11:26:42 +0300

    enumrate in for loop

---
+++ march/enumloop.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""
+enumurate in for loop
+"""
+fruits = ["apple", "banana", "orange"]
+for index, fruit in enumerate(fruits):
+    print("At position {}, I found a {}".format(index, fruit))
