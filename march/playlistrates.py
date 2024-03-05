2e7da0673ffc27b58c649ca05c93ed80f38d1099
samke-glitch <sammythemwa@gmail.com>
2024-03-05 12:07:58 +0300

    ratings

---
+++ march/playlistrates.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Write a while loop to display the values of the Rating
+of an album playlist stored in the list PlayListRatings.
+If the score is less than 6, exit the loop.
+The list PlayListRatings is given by:
+PlayListRatings = [10, 9.5, 10, 8, 7.5, 5, 10, 10]
+"""
+PlayListRatings = [10, 9.5, 10, 8, 7.5, 5, 10, 10]
+i = 0
+rating = PlayListRatings[0]
+while(i < len(PlayListRatings) and rating >= 6):
+    print(rating)
+    i += 1
+    rating = PlayListRatings[i]
