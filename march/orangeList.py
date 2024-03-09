280d58641516486a7e1af5a2bcd97fbc7eafea3e
samke-glitch <sammythemwa@gmail.com>
2024-03-09 20:29:52 +0300

    commenting on oranges module

--- march/orangeList.py
+++ march/orangeList.py
@@ -7,6 +7,7 @@ Stop and exit the loop if the value on the list is not 'orange':
 squares = ['orange', 'orange', 'purple', 'blue ', 'orange']
 new_squares = []
 i = 0
+# While loop to iterate through the list squares 
 while(i < len(squares) and squares[i] == 'orange'):
     new_squares.append(squares[i])
     i = i + 1
