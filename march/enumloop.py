11186e07fef0479a641547539e7bac8a90d730bd
samke-glitch <sammythemwa@gmail.com>
2024-03-09 20:31:14 +0300

    enumerate module

--- march/enumloop.py
+++ march/enumloop.py
@@ -2,6 +2,7 @@
 """
 enumurate in for loop
 """
+# enumarate gives you the index together with the element in that index like it's key and value
 fruits = ["apple", "banana", "orange"]
 for index, fruit in enumerate(fruits):
     print("At position {}, I found a {}".format(index, fruit))
