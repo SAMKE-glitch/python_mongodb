fda4b932354d960bdf408fcace344fc5062c3e08
samke-glitch <sammythemwa@gmail.com>
2024-03-13 12:22:02 +0300

    fileio

--- march/filePython/readfile.py
+++ march/filePython/readfile.py
@@ -5,7 +5,8 @@ Reading and writing files
 # write line to file
 file = "example.txt"
 with open(file, 'w') as writeFile:
-    writeFile.write("this is my first test")
+    writeFile.write("this is my first test\n")
+    writeFile.write("My second time writing\n")
 
 # read file
 with open(file, 'r') as readFile:
