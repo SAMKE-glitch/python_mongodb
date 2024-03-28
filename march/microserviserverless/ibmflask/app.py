404b63d04598a6e4baa6e9aaa1690b496c14493b
samke-glitch <sammythemwa@gmail.com>
2024-03-28 17:47:54 +0300

    json object

--- march/microserviserverless/ibmflask/app.py
+++ march/microserviserverless/ibmflask/app.py
@@ -2,11 +2,13 @@
 """
 creating a basic flask app
 """
-from flask import Flask
+from flask import Flask, jsonify
 
 
 app = Flask(__name__)
 @app.route("/")
 def hello_world():
     # serialized type
-    return {"message": "My first Application in action from IBM!"}
+    # return {"message": "My first Application in action from IBM!"}
+    # jsonify object
+    return jsonify(message="Json object")
